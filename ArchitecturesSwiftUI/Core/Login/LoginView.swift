7b9ccecf5b7c3db4ddc53f912919707eeb46f490
Frédéric Helfer <fre.helfer@gmail.com>
2023-02-05 11:01:54 -0300

    init

---
+++ ArchitecturesSwiftUI/Core/Login/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  LoginView.swift
+//  ArchitecturesSwiftUI
+//
+//  Created by Frédéric Helfer on 05/02/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color.gray.opacity(0.2).ignoresSafeArea()
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
