cb608247824e64bfee550d026325665507c72dcf
Frédéric Helfer <fre.helfer@gmail.com>
2023-02-05 10:58:55 -0300

    Initial Commit

---
+++ ArchitecturesSwiftUI/ArchitecturesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitecturesSwiftUIApp.swift
+//  ArchitecturesSwiftUI
+//
+//  Created by Frédéric Helfer on 05/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ArchitecturesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
